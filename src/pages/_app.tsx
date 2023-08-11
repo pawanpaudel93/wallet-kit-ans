b0a61a52bb6bb7a0bda155419ded2e67ace94e5a
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-11 18:12:10 +0545

    Initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ArweaveWalletKit } from "arweave-wallet-kit";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ArweaveWalletKit
+      config={{
+        permissions: ["ACCESS_ADDRESS"],
+        ensurePermissions: true,
+        appInfo: {
+          name: "Arweave Wallet Kit Bug",
+        },
+      }}
+      theme={{
+        accent: { r: 166, g: 42, b: 34 },
+      }}
+    >
+      <Component {...pageProps} />
+    </ArweaveWalletKit>
+  );
 }
